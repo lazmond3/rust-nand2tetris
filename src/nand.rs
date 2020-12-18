740bab40311a56c2dc0f1893a0f8ef676f17750c
= <=>
2020-12-18 13:10:36 +0900

    update nand

--- src/nand.rs
+++ src/nand.rs
@@ -2,15 +2,9 @@ use crate::bit::Bit;
 use crate::bit::Bit::{I, O};
 
 pub fn nand(a: Bit, b: Bit) -> Bit {
-    match a {
-        O => match b {
-            O => I,
-            I => I,
-        },
-        I => match b {
-            O => I,
-            I => O,
-        },
+    match a == b && a == I {
+        true => O,
+        false => I,
     }
 }
 
