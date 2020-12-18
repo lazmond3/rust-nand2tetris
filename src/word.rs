2094b1727192fb3a7ad7b09945131f2507993a07
= <=>
2020-12-19 00:12:46 +0900

    impl index and index_mut and add one_bit_word function

--- src/word.rs
+++ src/word.rs
@@ -1,17 +1,39 @@
 use crate::bit::Bit;
-use std::ops::Index;
+use std::default::Default;
+use std::ops::{Index, IndexMut};
 
-pub struct Word([Bit; 16]);
+const BIT_WIDTH: usize = 16;
+
+#[derive(Default, IndexMut)]
+pub struct Word([Bit; BIT_WIDTH]);
 
 impl Index<usize> for Word {
     type Output = Bit;
 
     fn index(&self, index: usize) -> &Self::Output {
-        if 15 < index {
+        if (BIT_WIDTH - 1) < index {
             panic!(format!("index fail: {} is out of range.", index))
         }
         &self.0[index]
     }
+}
+
+impl IndexMut<usize> for Word {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        if (BIT_WIDTH - 1) < index {
+            panic!(format!("index fail: {} is out of range.", index))
+        }
+        self.0.index_mut(index)
+    }
+}
+
+fn one_bit_word(index: usize) -> Word {
+    if (BIT_WIDTH - 1) < index {
+        panic!(format!("index fail: {} is out of range.", index))
+    }
+    let ar: Word = Default::default();
+    ar[index] = Bit::I;
+    ar
 }
 
 #[cfg(test)]
