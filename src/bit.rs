e90dc040dc5d85b37773904960790aa72de72672
= <=>
2020-12-18 13:00:00 +0900

    add bit partialeq

--- src/bit.rs
+++ src/bit.rs
@@ -1,3 +1,4 @@
+use std::cmp::PartialEq;
 use std::fmt;
 
 #[derive(Debug)]
@@ -13,5 +14,20 @@ impl fmt::Display for Bit {
             Bit::I => "I",
         };
         write!(f, "{}", v)
+    }
+}
+
+impl PartialEq for Bit {
+    fn eq(&self, other: &Self) -> bool {
+        match *self {
+            Bit::O => match *other {
+                Bit::O => true,
+                Bit::I => false,
+            },
+            Bit::I => match *other {
+                Bit::O => false,
+                Bit::I => true,
+            },
+        }
     }
 }
--- src/nand.rs
+++ src/nand.rs
@@ -13,3 +13,17 @@ pub fn nand(a: Bit, b: Bit) -> Bit {
         },
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::nand;
+    use super::Bit::{I, O};
+
+    #[test]
+    fn for_nand() {
+        assert_eq!(nand(O, O), I);
+        assert_eq!(nand(O, I), I);
+        assert_eq!(nand(I, O), I);
+        assert_eq!(nand(I, I), O);
+    }
+}
