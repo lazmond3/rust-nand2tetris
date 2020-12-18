ff176c135385bedbf2afdd415c23fb9fa92eb816
= <=>
2020-12-19 00:15:31 +0900

    add word test

--- src/word.rs
+++ src/word.rs
@@ -2,9 +2,9 @@ use crate::bit::Bit;
 use std::default::Default;
 use std::ops::{Index, IndexMut};
 
-const BIT_WIDTH: usize = 16;
+pub const BIT_WIDTH: usize = 16;
 
-#[derive(Default, IndexMut)]
+#[derive(Default)]
 pub struct Word([Bit; BIT_WIDTH]);
 
 impl Index<usize> for Word {
@@ -27,27 +27,26 @@ impl IndexMut<usize> for Word {
     }
 }
 
-fn one_bit_word(index: usize) -> Word {
+pub fn one_bit_word(index: usize) -> Word {
     if (BIT_WIDTH - 1) < index {
         panic!(format!("index fail: {} is out of range.", index))
     }
-    let ar: Word = Default::default();
+    let mut ar: Word = Default::default();
     ar[index] = Bit::I;
     ar
 }
 
 #[cfg(test)]
 mod tests {
-    use super::Bit;
+    use super::one_bit_word;
     use super::Bit::{I, O};
     use super::Word;
+    use super::{Bit, BIT_WIDTH};
 
     #[test]
     fn for_word() {
-        let bit_array_01: [Bit; 16] = [I, O, O, O, O, O, O, O, O, O, O, O, O, O, O, O];
-        let bit_array_15: [Bit; 16] = [O, O, O, O, O, O, O, O, O, O, O, O, O, O, O, I];
-        let word_01 = Word(bit_array_01);
-        let word_15 = Word(bit_array_15);
+        let word_01: Word = one_bit_word(0);
+        let word_15: Word = one_bit_word(BIT_WIDTH - 1);
         assert_eq!(word_01[0], I);
         assert_eq!(word_01[1], O);
         assert_eq!(word_15[0], O);
