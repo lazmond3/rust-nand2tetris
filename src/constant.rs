31096c30bb1eae758c71e6cec2af4b92aabae1d6
= <=>
2020-12-19 23:55:04 +0900

    bit shift dir

--- src/constant.rs
+++ src/constant.rs
@@ -1,4 +1,4 @@
 pub const BIT_WIDTH: usize = 16;
 pub const MAX_VALUE: usize = ((1 >> BIT_WIDTH) - 1) as usize;
-// 8K RAM
-pub const RAM_WORDS_NUM: usize = 8 * (1 >> 10);
+// 8K words RAM
+pub const RAM_WORDS_NUM: usize = 8 * (1 << 10);
