1be267e6697faf31d8a681ed2dd50b5ece860fde
= <=>
2020-12-19 10:45:37 +0900

    change internal but to word in util

--- src/util.rs
+++ src/util.rs
@@ -7,7 +7,7 @@ use std::default::Default;
 use std::convert::{TryFrom, TryInto};
 use std::result::Result;
 
-pub fn convert_vec_to_internal_word(a: Vec<Bit>) -> InternalWord {
+pub fn convert_vec_to_word(a: Vec<Bit>) -> Word {
     let boxed_slice = a.clone().into_boxed_slice();
     let boxed_array: Box<InternalWord> = match boxed_slice.try_into() {
         Ok(ba) => ba,
@@ -19,7 +19,7 @@ pub fn convert_vec_to_internal_word(a: Vec<Bit>) -> InternalWord {
             )
         }
     };
-    *boxed_array
+    Word::new(*boxed_array)
 }
 
 //https://stackoverflow.com/questions/29570607/is-there-a-good-way-to-convert-a-vect-to-an-array
@@ -50,7 +50,7 @@ mod tests {
     fn for_convert_vec_to_internal_word() {
         let word_00: Word = one_bit_word(0);
         let vec = word_00.internal().to_vec();
-        let word_from_vec = Word::new(convert_vec_to_internal_word(vec));
+        let word_from_vec = convert_vec_to_word(vec);
 
         assert_eq!(word_00, word_from_vec);
         // for i in 0..BIT_WIDTH {
