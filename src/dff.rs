850acc0952d70433484c32806b3944a3d92acf18
= <=>
2020-12-19 23:27:50 +0900

    add dff

---
+++ src/dff.rs
@@ -0,0 +1,23 @@
+use crate::bit::Bit;
+use crate::clock::{Clock, ClockState};
+
+pub struct Dff {
+    state_past: Bit,
+    state_next: Bit,
+}
+
+impl Dff {
+    fn new() -> Self {
+        Dff {
+            state_past: Bit::O,
+            state_next: Bit::O,
+        }
+    }
+
+    fn input(&mut self, a: Bit, clock: &Clock) {
+        if clock.state() == Bit::I {
+            self.state_past = self.state_next.clone();
+            self.state_next = a;
+        }
+    }
+}
