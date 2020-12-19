93928e48183b145d73ee80942a0703dc8d7938c9
= <=>
2020-12-20 00:04:40 +0900

    add ram functions delete comment

--- src/ram.rs
+++ src/ram.rs
@@ -1,5 +1,6 @@
 use crate::constant::RAM_WORDS_NUM;
 use crate::word::Word;
+use std::convert::TryInto;
 use std::ops::{Index, IndexMut};
 
 pub type InternalRam = [Word; RAM_WORDS_NUM];
@@ -7,7 +8,6 @@ pub struct Ram(InternalRam);
 
 impl Ram {
     pub fn new() -> Self {
-        // Ram([Word::new_empty(); RAM_WORDS_NUM])
         Ram([Word::new_empty(); RAM_WORDS_NUM])
     }
     pub fn internal(&self) -> &InternalRam {
@@ -15,6 +15,27 @@ impl Ram {
     }
     pub fn to_vec(&self) -> Vec<Word> {
         (*self).internal().to_vec()
+    }
+    pub fn from_vec_word(a: Vec<Word>) -> Self {
+        if a.len() != RAM_WORDS_NUM {
+            panic!(format!(
+                "range error: vector range:{} is mismatched to RAM_WORDS_NUM:{}.",
+                a.len(),
+                RAM_WORDS_NUM
+            ))
+        }
+        let boxed_slice = a.clone().into_boxed_slice();
+        let boxed_array: Box<InternalRam> = match boxed_slice.try_into() {
+            Ok(ba) => ba,
+            Err(_) => {
+                panic!(
+                    "Expected a Vec of length {} but it was {}.",
+                    RAM_WORDS_NUM,
+                    a.len()
+                )
+            }
+        };
+        Ram(*boxed_array)
     }
 }
 
