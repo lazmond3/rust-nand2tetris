d3329435b52516e30d3bca11ffdb6a12834db30e
= <=>
2020-12-20 10:45:03 +0900

    update constan

--- src/constant.rs
+++ src/constant.rs
@@ -1,4 +1,5 @@
 pub const BIT_WIDTH: usize = 16;
 pub const MAX_VALUE: usize = ((1 >> BIT_WIDTH) - 1) as usize;
 // 8K words RAM
-pub const RAM_WORDS_NUM: usize = 8 * (1 << 10);
+pub const RAM_WORDS_NUM: usize = 4 * (1 << 10);
+pub const ROM_RAM_SIZE: usize = 8;
