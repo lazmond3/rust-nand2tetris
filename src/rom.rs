b6e3dc0305d8f056272cd24145077d16f637381f
= <=>
2020-12-20 12:23:33 +0900

    delete not used test

--- src/rom.rs
+++ src/rom.rs
@@ -3,7 +3,7 @@ use crate::ram::Ram;
 use std::fs::File;
 use std::io::Read;
 use std::io::{self, BufRead, BufReader};
-use std::path::Path;
+// use std::path::Path;
 
 struct Rom {
     rams: [Ram; ROM_RAM_SIZE],
@@ -24,29 +24,6 @@ impl Rom {
             println!("line: {} ", line.unwrap());
         }
     }
-}
-
-fn readme(file_name: &str) {
-    let file = File::open(file_name).expect(&format!("failed open file: {}", file_name));
-    let mut reader = BufReader::new(file);
-
-    let mut buf = &mut [b'\0'; 100];
-    reader.read(buf);
-    let vec = buf.to_vec();
-
-    let st = std::string::String::from_utf8(vec).expect(&format!("failed to from utf8"));
-
-    println!("in readme: readed string: {}", st);
-}
-
-// The output is wrapped in a Result to allow matching on errors
-// Returns an Iterator to the Reader of the lines of the file.
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where
-    P: AsRef<Path>,
-{
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
 }
 
 #[cfg(test)]
@@ -70,38 +47,5 @@ mod tests {
         let my_string = std::string::String::from("hello string &str");
         println!("print_test v:  my_string string: {}", my_string);
         println!("print_test v:  my_string &String is &str?: {}", &my_string)
-    }
-
-    // read test
-
-    #[test]
-    /*
-        hello3
-        hello3
-        hello3
-        he
-    */
-    fn read_me_test() {
-        // buffer read
-        readme("sample.txt")
-    }
-
-    #[test]
-    /*
-        line : hello3
-        line : hello3
-        line : hello3
-        line : hello3
-        line : hello3
-        line : hello3
-        line :
-        test rom::tests::read_test ... ok
-    */
-    fn read_test() {
-        let lines = read_lines("sample.txt").expect(&format!("failed to give lines."));
-        for line in lines {
-            let u = line.unwrap();
-            println!("line : {}", u);
-        }
     }
 }
